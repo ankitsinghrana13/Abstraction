58b6b466b73814bdfa298e0ee0e68f1a41af7e36
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:33:11 +0800

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace ConsoleApp7
+{
+
+    abstract class progrm
+    {
+        public string name;
+        public abstract void data();
+
+        public void inform(string name)
+        {
+            this.name = name;
+            Console.WriteLine("name=" + name);
+
+        }
+
+    }
+        class property : progrm
+        {
+       public override void data()
+        {
+            Console.WriteLine("this class belongs to property");
+            
+        }
+
+        }
+    
+        class program
+        {
+            static void Main(string[] arg)
+            {
+
+            property obj = new property();
+            obj.data();
+            obj.inform("hashi");
+
+           
+            
+
+
+
+
+
+
+        }
+        }
+    
+}
